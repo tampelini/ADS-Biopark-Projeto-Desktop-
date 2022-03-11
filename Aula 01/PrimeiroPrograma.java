1f21f023540d1eac0b80b74a5c146dd0ab809ad0
Leonardo Garcia Tampelini <tampelini@gmail.com>
2022-03-11 13:35:42 -0300

    Exemplo de leitura e escrita em java

---
+++ Aula 01/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        
+        //Lê a partir da linha de comando
+    Scanner sc1 = new Scanner(System.in);
+    System.out.println("Entrada: ");
+    String texto = sc1.nextLine();
+
+    System.out.println(">>" + texto);
+    sc1.close();
+    }
+}
